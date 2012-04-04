26ca3c5566eed108274fbd2d6f5f428748a1d5eb
Keir Nellyer <keir@keir-nellyer.co.uk>
2012-04-04 16:47:10 +0100

    Should really store the version for easy editing and reference.

--- My First Game/src/me/iKeirNez/FirstGame/Main.java
+++ My First Game/src/me/iKeirNez/FirstGame/Main.java
@@ -5,14 +5,17 @@ import javax.swing.JFrame;
 public class Main {
 	
 	public static Display f = new Display();
+	
 	public static int w = 600;
 	public static int h = 400;
+	public static String version = "1.0";
+	
 	public static void main(String args[]){
 		f.setSize(w, h);
 		f.setResizable(true);
 		f.setVisible(true);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setTitle("iKeirNez's First Java Game - Version 1.0");
+		f.setTitle("iKeirNez's First Java Game - v." + version);
 		f.setLocationRelativeTo(null);
 		f.setAlwaysOnTop(false);
 	}
