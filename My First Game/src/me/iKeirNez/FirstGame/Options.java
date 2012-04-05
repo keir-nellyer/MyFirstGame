19aa8f5a93c6b1f3ac5448543cba22c16aafa673
Keir Nellyer <keir@keir-nellyer.co.uk>
2012-04-05 16:28:00 +0100

    Update version for new feature.

--- My First Game/src/me/iKeirNez/FirstGame/Options.java
+++ My First Game/src/me/iKeirNez/FirstGame/Options.java
@@ -6,7 +6,7 @@ import java.awt.event.KeyEvent;
 public class Options {
 	
 	// Version of the "game"
-	public static double version = 0.2;
+	public static double version = 0.3;
 	
 	// Width and Height of the Rectangle
 	public static int CharacterWidth = 24;
